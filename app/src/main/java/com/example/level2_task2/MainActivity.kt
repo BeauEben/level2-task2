38c16d999833037c0cd48584925329bb08f1689f
Beau Wijkstra <beau.wijkstra@hva.nl>
2020-10-18 22:19:49 +0200

    make all the classes and implement the recyclerview

--- app/src/main/java/com/example/level2_task2/MainActivity.kt
+++ app/src/main/java/com/example/level2_task2/MainActivity.kt
@@ -2,14 +2,36 @@ package com.example.level2_task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.level2_task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private val questions = arrayListOf<Question>()
+    private val questionAdapter = QuestionAdapter(questions)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews(){
+        binding.rvQuestions.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        binding.rvQuestions.adapter = questionAdapter
+
+        binding.rvQuestions.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+
+        addQuestions()
+    }
+
+    private fun addQuestions(){
+        questions.add(Question("Is this a test question?", true))
+        questionAdapter.notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/com/example/level2_task2/Question.kt
@@ -0,0 +1,6 @@
+package com.example.level2_task2
+
+data class Question (
+    var questionText: String,
+    var answer: Boolean
+)
