12f95e27f8e780212f81435e90fc2a62d046f992
Beau Wijkstra <beau.wijkstra@hva.nl>
2020-10-18 22:44:53 +0200

    Added functionality

--- app/src/main/java/com/example/level2_task2/MainActivity.kt
+++ app/src/main/java/com/example/level2_task2/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.level2_task2
 
+import android.content.ClipData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.level2_task2.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -27,11 +30,53 @@ class MainActivity : AppCompatActivity() {
 
         binding.rvQuestions.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
 
+
+        createItemTouchHelper().attachToRecyclerView(binding.rvQuestions)
         addQuestions()
     }
 
     private fun addQuestions(){
-        questions.add(Question("Is this a test question?", true))
+        questions.add(Question("A cow is an animal", true))
+        questions.add(Question("An animal is a cow", false))
         questionAdapter.notifyDataSetChanged()
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper{
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                if (direction == ItemTouchHelper.LEFT) {
+                    if (!questions[position].answer){
+                        questions.removeAt(position)
+                        questionAdapter.notifyDataSetChanged()
+                    }
+                    if (questions[position].answer){
+                        Snackbar.make(binding.qzInstructions, "Wrong!", Snackbar.LENGTH_SHORT).show()
+                        questionAdapter.notifyDataSetChanged()
+                    }
+                }
+
+                if (direction == ItemTouchHelper.RIGHT) {
+                    if (!questions[position].answer){
+                        Snackbar.make(binding.qzInstructions, "Wrong!", Snackbar.LENGTH_SHORT).show()
+                        questionAdapter.notifyDataSetChanged()
+                    }
+                    if (questions[position].answer){
+                        questions.removeAt(position)
+                        questionAdapter.notifyDataSetChanged()
+                    }
+                }
+            }
+        }
+        return ItemTouchHelper(callback)
     }
 }
